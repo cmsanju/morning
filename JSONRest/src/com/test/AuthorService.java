bf46510a181e92f28e5a2a6c6023527a12835fa3
gonesanjeeva <gonesanjeeva@localhost>
2020-12-25 09:40:28 +0530

    New Json project uploading

---
+++ JSONRest/src/com/test/AuthorService.java
@@ -0,0 +1,37 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/AuthorService")
+public class AuthorService {
+	
+	
+	@GET
+	@Path("/authors")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Person> getTrackInJSON() {
+		
+		List<Person> listPerson = new ArrayList<>();
+		
+		Person p1 = new Person();
+		p1.setId(1);;
+		p1.setName("Sunil");
+		
+		Person p2 = new Person();
+		p2.setId(2);
+		p2.setName("Jaffer");
+		
+		listPerson.add(p1);
+		listPerson.add(p2);
+		
+		return listPerson;
+	}
+	
+	
+}
